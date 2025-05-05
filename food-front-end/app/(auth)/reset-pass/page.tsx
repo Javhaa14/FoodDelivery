635c9bed9ff47ed8f38cb6149aa90b56efa99462
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-05 11:03:39 +0800

    suspense

--- food-front-end/app/(auth)/reset-pass/page.tsx
+++ food-front-end/app/(auth)/reset-pass/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { Step6 } from "../components/step6";
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
   return (
     <div className="flex pb-[110px] w-full h-full justify-center items-center ">
       <div className="flex flex-col justify-center items-start gap-6 p-10">
-        <Step6 name={blah.name} down={blah.down} />
+        <Suspense>
+          <Step6 name={blah.name} down={blah.down} />
+        </Suspense>
       </div>
     </div>
   );
